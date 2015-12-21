398a561272d568263375ac9147f1cce3000feb62
A. Tock <atock@gohealth.com>
2015-12-21 11:30:56 -0600

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+  resources :posts
+ #root to: "posts#index"
+
+devise_scope :user do
+  authenticated :user do
+   root 'posts#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+   root 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
+end
